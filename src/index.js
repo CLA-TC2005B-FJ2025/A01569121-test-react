4a227916d116fd7628ecde3d5fc4c365b2498ef7
Myriam Muruato <149907407+mmuruato@users.noreply.github.com>
2025-03-25 16:02:36 +0000

    Hola mundo

--- src/index.js
+++ src/index.js
@@ -13,10 +13,10 @@ function App() {
     
     // return <h1>Hola Mundo</h1>;
 
-    let mensaje = "Hola";
+    let mensaje = "Hola Mundo";
     if (Math.random() < 0.5 )
         mensaje = "Hola Mundo" 
-    return <h1>{mensaje}</h1>
+    return <h2>{mensaje}</h2>
 }
 
 // 5) Mostrar el componente en la pantalla
